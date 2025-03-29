4f3ca8ce49cf946522592a332dd76b28e81dc506
davalavil <47474842+davalavil@users.noreply.github.com>
2025-03-29 20:14:15 +0100

    Create verbs.js

---
+++ js/verbs.js
@@ -0,0 +1,39 @@
+// --- IMPORTANTE ---
+// Reemplaza este contenido de ejemplo con TU PROPIA LISTA DE VERBOS.
+// Asegúrate de que la variable se llame 'verbList' y siga el formato:
+// [Infinitivo, Pasado Simple, Pasado Participio, Traducción, Tipo]
+
+const verbList = [
+    // [Infinitive, Past Simple, Past Participle, Translation, Type]
+    ["be", "was/were", "been", "ser/estar", "irregular"],
+    ["go", "went", "gone", "ir", "irregular"],
+    ["play", "played", "played", "jugar", "regular"],
+    ["study", "studied", "studied", "estudiar", "regular"],
+    ["eat", "ate", "eaten", "comer", "irregular"],
+    ["drink", "drank", "drunk", "beber", "irregular"],
+    ["work", "worked", "worked", "trabajar", "regular"],
+    ["write", "wrote", "written", "escribir", "irregular"],
+    ["read", "read", "read", "leer", "irregular"], // Pronunciación diferente
+    ["see", "saw", "seen", "ver", "irregular"],
+    // ...
+    // ... Añade MUCHOS MÁS verbos aquí siguiendo el mismo formato
+    // ...
+    ["have", "had", "had", "tener/haber", "irregular"],
+    ["make", "made", "made", "hacer/fabricar", "irregular"],
+    ["do", "did", "done", "hacer", "irregular"],
+    ["say", "said", "said", "decir", "irregular"],
+    ["get", "got", "got/gotten", "conseguir/obtener", "irregular"], // gotten es más común en AmE
+    ["know", "knew", "known", "saber/conocer", "irregular"],
+    ["think", "thought", "thought", "pensar", "irregular"],
+    ["take", "took", "taken", "tomar/coger", "irregular"],
+    ["come", "came", "come", "venir", "irregular"],
+    ["want", "wanted", "wanted", "querer", "regular"],
+    ["look", "looked", "looked", "mirar", "regular"],
+    ["use", "used", "used", "usar", "regular"],
+    ["find", "found", "found", "encontrar", "irregular"],
+    ["give", "gave", "given", "dar", "irregular"],
+    ["tell", "told", "told", "decir/contar", "irregular"]
+];
+
+// Nota: Si tienes cientos de verbos, considera optimizaciones futuras
+// como cargar solo una selección o implementar paginación.
